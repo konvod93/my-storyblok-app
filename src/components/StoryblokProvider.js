12917d939c8b08ed2bb025d9581acfdf74516442
Konstantin <57582571+konvod93@users.noreply.github.com>
2024-01-15 20:26:02 +0200

    added imports of components in StoryblokProvider

--- src/components/StoryblokProvider.js
+++ src/components/StoryblokProvider.js
@@ -1,11 +1,25 @@
 /** 1. Tag it as a client component */
 "use client";
 import { storyblokInit, apiPlugin } from "@storyblok/react/rsc";
+
+/** Import your components */
+import Page from "./Page";
+import Teaser from "./Teaser";
+import Feature from "./Feature";
+import Grid from "./Grid";
+
+const components = {
+  feature: Feature,
+  grid: Grid,
+  teaser: Teaser,
+  page: Page,
+}
  
 /** 2. Initialize it as usual */
 storyblokInit({
   accessToken: "150d3yW7VYN3z79ov3KcxQtt",
   use: [apiPlugin],
+  components
 });
  
 export default function StoryblokProvider({ children }) {
