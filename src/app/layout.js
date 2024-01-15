13e1dcf23b53f4db5040ec9fdf359e283cd60d15
Konstantin <57582571+konvod93@users.noreply.github.com>
2024-01-15 20:09:57 +0200

    add Storyblok component to layout, updated home psge

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,6 +1,7 @@
 import { Inter } from 'next/font/google'
 import './globals.css'
 import { storyblokInit, apiPlugin } from "@storyblok/react/rsc";
+import StoryblokProvider from '../components/StoryblokProvider';
  
 storyblokInit({
   accessToken: "150d3yW7VYN3z79ov3KcxQtt",
@@ -16,8 +17,10 @@ export const metadata = {
 
 export default function RootLayout({ children }) {
   return (
-    <html lang="en">
-      <body className={inter.className}>{children}</body>
-    </html>
-  )
+    <StoryblokProvider>
+      <html lang="en">
+        <body className={inter.className}>{children}</body>
+      </html>
+    </StoryblokProvider>
+  );
 }
