a6726c30ba82c57b5cdd8a817ec63a9e19a80dcf
Konstantin <57582571+konvod93@users.noreply.github.com>
2024-01-19 20:27:23 +0200

    added "no-cache"

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,7 +1,9 @@
-import { Inter } from 'next/font/google'
-import './globals.css'
+import { Inter } from 'next/font/google';
+import './globals.css';
 import { storyblokInit, apiPlugin } from "@storyblok/react/rsc";
 import StoryblokProvider from '../components/StoryblokProvider';
+import Navigation from '@/components/Navigation';
+import Footer from '@/components/Footer';
  
 storyblokInit({
   accessToken: "150d3yW7VYN3z79ov3KcxQtt",
@@ -19,7 +21,11 @@ export default function RootLayout({ children }) {
   return (
     <StoryblokProvider>
       <html lang="en">
-        <body className={inter.className}>{children}</body>
+        <body className={inter.className}>
+          <Navigation />
+          {children}
+          <Footer />
+        </body>
       </html>
     </StoryblokProvider>
   );
--- src/app/page.js
+++ src/app/page.js
@@ -17,5 +17,5 @@ export async function fetchData() {
   let sbParams = { version: "draft" };
  
   const storyblokApi = getStoryblokApi();
-  return storyblokApi.get(`cdn/stories/home`, sbParams);
+  return storyblokApi.get(`cdn/stories/home`, sbParams, {cache:"no-cache"});
 }
